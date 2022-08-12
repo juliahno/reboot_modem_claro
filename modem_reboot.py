8f108de02afa60a0471f6605edd2082f434129ec
Juliano <43688750+juliahno@users.noreply.github.com>
2022-08-12 18:46:26 -0300

    Commit inicial
    Commit inicial

---
+++ modem_reboot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Modem Claro Kaon CG3000
+
+import requests
+
+
+MD5_USUARIO = "00000000000000000000000000000000"
+MD5_SENHA = "00000000000000000000000000000000"
+
+DADOS = {
+    'login_id': MD5_USUARIO,
+    'login_pw': MD5_SENHA
+}
+
+URL_LOGIN = "http://192.168.0.1/logincheck.cmd"
+
+sessao = requests.Session()
+
+try:
+    resposta_login = sessao.post(URL_LOGIN, data=DADOS, timeout=3)
+    resposta_login.raise_for_status()
+except Exception as e:
+    print("Erro:", e)
+    exit()
+
+URL_REBOOT = "http://192.168.0.1/rebootinfo.cgi?path_name=resetrouter.html"
+
+sessao = requests.Session()
+try:
+    resposta_reboot = sessao.get(URL_REBOOT, timeout=3)
+    resposta_reboot.raise_for_status()
+except Exception as e:
+    print("Erro:", e)
+    exit()
